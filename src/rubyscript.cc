7ae09dd01bcfc8f6e37aa47e7b2c27432ee35341
Henning Meyer <hmeyer@gmx.eu>
2011-05-02 13:23:36 +0200

    csg and translate

--- src/rubyscript.cc
+++ src/rubyscript.cc
@@ -2,6 +2,8 @@
 
 #include "node.h"
 #include "primitives.h"
+#include "csgops.h"
+#include "transform.h"
 #include <rice/Array.hpp>
 #include <rice/Hash.hpp>
 #include <rice/Symbol.hpp>
@@ -31,17 +33,51 @@ AbstractNode::Pointer RBAbstractNode::getNode() const {
   return node;
 }
 
-void Array2Point3D(const Array &a, Point3D &p) {
-  p = Point3D();
-  try {
-    Array::const_iterator it = a.begin();
-    if (it != a.end()) p.x = it->value(); ++it;
-    if (it != a.end()) p.y = it->value(); ++it;
-    if (it != a.end()) p.z = it->value();
-  } catch( std::exception &e) {
-    std::cerr << "Array to Point3D warning:" << e.what() << std::endl;
-  }    
+AbstractNode::NodeList Array2NodeList(const Array &a) {
+  AbstractNode::NodeList list;
+  for(Array::const_iterator it = a.begin(); it != a.end(); ++it) {
+    try {
+      list.push_back(from_ruby<RBAbstractNode>(it->value()).getNode());
+    } catch( std::exception &e) {
+      std::cerr << "Array2NodeList warning:" << e.what() << std::endl;
+    }        
+  }
+  return list;
 }
+
+template<csg_type_e type> 
+class RBCSGNode: public RBAbstractNode {
+public:
+  RBCSGNode(const Array &a) {
+    node = make_shared<CsgNode>(type, Array2NodeList(a));
+  }
+};
+
+typedef RBCSGNode< CSG_TYPE_UNION > RBUnionNode;
+typedef RBCSGNode< CSG_TYPE_DIFFERENCE > RBDifferenceNode;
+typedef RBCSGNode< CSG_TYPE_INTERSECTION > RBIntersectionNode;
+
+
+Float3 Array2Float3(const Array &a) {
+  Float3 p;
+  try {
+    int i=0;
+    for(Array::const_iterator it = a.begin(); it!=a.end() && i < p.static_size; ++it,++i)
+      p[i] = from_ruby<double>(it->value());
+  } catch( std::exception &e) {
+    std::cerr << "Array to Float3 warning:" << e.what() << std::endl;
+  }
+  return p;
+}
+
+class RBTranslateNode: public RBAbstractNode {
+public:
+  RBTranslateNode(const Array &va, const Array &a) {
+    node = make_shared<TransformTranslateNode>(Array2Float3(va), Array2NodeList(a));
+  }
+};
+
+
 
 PrimitiveNode::Accuracy getAcc(void) {
   PrimitiveNode::Accuracy acc;
@@ -57,10 +93,8 @@ PrimitiveNode::Accuracy getAcc(void) {
 
 class RBCubeNode: public RBAbstractNode {
 public:
-  RBCubeNode(const Array &adim, bool center) {
-    Point3D dim;
-    Array2Point3D(adim, dim);
-    node = make_shared<CubeNode>(dim, center);
+  RBCubeNode(const Array &adim, bool center) {  
+    node = make_shared<CubeNode>(Array2Float3(adim), center);  
   }
 };
 
@@ -76,6 +110,18 @@ RubyScript::RubyScript():status(0) {
   ruby_init();
   Data_Type<RBAbstractNode> rb_AbstractNode =
     define_class<RBAbstractNode>("AbstractNode");
+  Data_Type<RBUnionNode> rb_UnionNode =
+    define_class<RBUnionNode, RBAbstractNode>("Union")
+    .define_constructor(Constructor<RBUnionNode, const Array>(),(Arg("children")));
+  Data_Type<RBDifferenceNode> rb_DifferenceNode =
+    define_class<RBDifferenceNode, RBAbstractNode>("Difference")
+    .define_constructor(Constructor<RBDifferenceNode, const Array>(),(Arg("children")));
+  Data_Type<RBIntersectionNode> rb_IntersectionNode =
+    define_class<RBIntersectionNode, RBAbstractNode>("Intersection")
+    .define_constructor(Constructor<RBIntersectionNode, const Array>(),(Arg("children")));    
+  Data_Type<RBTranslateNode> rb_TranslateNode =
+    define_class<RBTranslateNode, RBAbstractNode>("Translate")
+    .define_constructor(Constructor<RBTranslateNode, const Array, const Array>(),(Arg("vector"),Arg("children")));    
   Data_Type<RBCubeNode> rb_CubeNode =
     define_class<RBCubeNode, RBAbstractNode>("Cube")
     .define_constructor(Constructor<RBCubeNode, const Array, bool>(),(Arg("dim")=Array(),Arg("center")=false));
